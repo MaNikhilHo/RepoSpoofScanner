11432bbca9a2ce64f651d172c1a59ac51bf12afe
nilaamaharjan <nima00015@uni-saarland.de>
2025-03-24 22:47:52 +0100

    updated with lock

--- repospoofscanner/core/__init__.py
+++ repospoofscanner/core/__init__.py
@@ -2,9 +2,4 @@ from .scanner import RepoSpoofScanner
 from .parsers import parse_package_json, parse_requirements_txt
 from .registries import RegistryValidator
 
-__all__ = [
-    'RepoSpoofScanner',
-    'parse_package_json',
-    'parse_requirements_txt',
-    'RegistryValidator'
-]
+__all__ = ['RepoSpoofScanner', 'parse_package_json', 'parse_requirements_txt', 'RegistryValidator']
